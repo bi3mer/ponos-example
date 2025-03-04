c1270b7c9e69628d79ba30079e03b291d5e63393
bi3mer <colan@comcast.net>
2025-03-04 16:44:21 -0500

    added try catch around socket connection to close the socket if their is an error on this side or on the ponos side

--- Mario/socket_server.py
+++ Mario/socket_server.py
@@ -8,43 +8,47 @@ import os
 
 def server(host='localhost', port=8000):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen(1)
-        print(f"Server listening on {host}:{port}")
+        try:
+            s.bind((host, port))
+            s.listen(1)
+            print(f"Server listening on {host}:{port}")
 
-        while True:
-            conn, addr = s.accept()
-            print(f"Connected by {addr}")
+            while True:
+                conn, addr = s.accept()
+                print(f"Connected by {addr}")
 
-            with conn:
-                while True:
-                    cmd = conn.recv(1024)
-                    if not cmd:
-                        break
+                with conn:
+                    while True:
+                        cmd = conn.recv(1024)
+                        if not cmd:
+                            break
 
-                    if cmd == b"config":
-                        with open('config.json', 'r') as f:
-                            conn.sendall(bytes(f.read(), 'utf-8'))
-                    elif cmd == b'levels':
-                        lvls = []
-                        for file_name in os.listdir('levels'):
-                            with open(os.path.join('levels', file_name), 'r') as f:
-                                lvls.append(f.readlines())
+                        if cmd == b"config":
+                            with open('config.json', 'r') as f:
+                                conn.sendall(bytes(f.read(), 'utf-8'))
+                        elif cmd == b'levels':
+                            lvls = []
+                            for file_name in os.listdir('levels'):
+                                with open(os.path.join('levels', file_name), 'r') as f:
+                                    lvls.append(f.readlines())
 
-                        conn.sendall((dumps(lvls)+'EOF').encode())
-                    elif cmd[:6] == b'assess':
-                        lvl = loads(cmd[6:].decode('utf-8'))
-                        return_data = {
-                            'completability': percent_playable(lvl),
-                            'linearity': percent_linearity(lvl),
-                            'leniency': percent_leniency(lvl),
-                        }
+                            conn.sendall((dumps(lvls)+'EOF').encode())
+                        elif cmd[:6] == b'assess':
+                            lvl = loads(cmd[6:].decode('utf-8'))
+                            return_data = {
+                                'completability': percent_playable(lvl),
+                                'linearity': percent_linearity(lvl),
+                                'leniency': percent_leniency(lvl),
+                            }
 
-                        conn.sendall(dumps(return_data).encode('utf-8'))
-                    else:
-                        print(f'Unrecognized command: {cmd}')
-                        error_message = bytes(f'Unrecognized command: {cmd}', 'utf-8')
-                        conn.sendall(error_message)
+                            conn.sendall(dumps(return_data).encode('utf-8'))
+                        else:
+                            print(f'Unrecognized command: {cmd}')
+                            error_message = bytes(f'Unrecognized command: {cmd}', 'utf-8')
+                            conn.sendall(error_message)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            s.close()
 
 if __name__ == "__main__":
     server()
